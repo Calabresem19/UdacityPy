ae855fd4883e4ddd964c6cda83994ac0fddfb2b0
mike <mikeclbrs@gmail.com>
2017-02-05 15:42:30 -0500

    first commit

---
+++ rename.py
@@ -0,0 +1,6 @@
+import os
+
+def rename():
+    path = os.listdir("/home/mike/UdacityPy/pictures")
+    print path
+rename()
---
+++ takeBreak.py
@@ -0,0 +1,10 @@
+import webbrowser
+import time
+
+
+count = 0
+print ("this program started at "+time.ctime())
+while(count <  3):
+    time.sleep(60*60^2)
+    webbrowser.open("https://www.youtube.com/watch?v=kEGuHdKn0Lc")
+    count += 1
