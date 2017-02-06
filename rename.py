62adc22a975f91dcbd7504ea4ddb524ea54d5fae
mike <mikeclbrs@gmail.com>
2017-02-06 14:40:55 -0500

    starting class section

--- rename.py
+++ rename.py
@@ -1,6 +1,14 @@
 import os
 
 def rename():
-    path = os.listdir("/home/mike/UdacityPy/pictures")
-    print path
+    fileList = os.listdir("/home/mike/UdacityPy/pictures")
+
+    print fileList
+    savedPath = os.getcwd()
+    os.chdir("/home/mike/UdacityPy/pictures")
+    for fileName in fileList:
+        os.rename(fileName, fileName.translate(None,"1234567890") )
+    os.chdir(savedPath)
+
+
 rename()
